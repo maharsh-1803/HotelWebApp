3827815e9c95da94f917c425edbd825c5393758b
maharsh-1803 <maharshpatel1803@gmail.com>
2024-04-04 22:56:22 +0530

    backend done and frontEnd created

---
+++ api/routes/hotels.js
@@ -0,0 +1,22 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+import { countyByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotelController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+router.post('/',verifyAdmin,createHotel);
+//UPDATE
+router.put('/:id',verifyAdmin,updateHotel);
+//DELETE
+router.delete('/:id',verifyAdmin,deleteHotel)
+//GET
+router.get('/find/:id',getHotel)
+//GET ALL
+router.get('/',getHotels)
+router.get('/countByCity',countyByCity)
+router.get('/countByType',getHotels)
+
+export default router;
